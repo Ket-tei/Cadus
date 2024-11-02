02afa0c8bc8dd44ef1cfb1c4de63adab4e2198c7
Ket-tei <raph95004@gmail.com>
2024-11-02 17:50:12 +0100

    Mobile sized

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@
 * Injecte la navbar dans le body
 * 
 */
-function injectNavbar() {
+function injectDesktopNavbar() {
     const id = 'id="actual-page"'
 
     // Récupère le nom de la page actuelle
@@ -32,23 +32,23 @@ function injectNavbar() {
     <header>
         <nav>
             <ul class="menu">
-                <a href="./index.html"><img src="./assets/Logo_Cadus.png" alt="logo du site" /></a>
+                <a href="./html/index.html"><img src="./assets/Logo_Cadus.png" alt="logo du site" /></a>
                 <li ${id1}>
-                    <a href="./index.html">Acceuil</a>
+                    <a href="./html/index.html">Acceuil</a>
                 </li>
                 <li ${id2}>
-                    <a href="./quisommenous.html">Qui somme-nous ?</a>
+                    <a href="./html/quisommenous.html">Qui somme-nous ?</a>
                 </li>
                 <li ${id3}>
-                    <a href="./action.html">Nos actions</a>
+                    <a href="./html/action.html">Nos actions</a>
                 </li>
                 <li ${id4}>
-                    <a href="./aide.html">Nous aider</a>
+                    <a href="./html/aide.html">Nous aider</a>
                 </li>
             </ul>
             <ul class="contact-button">
                 <li>
-                    <a href="./contact.html">
+                    <a href="./html/contact.html">
                         <img src="./assets/arrow-circle.png" alt="arrow image">
                         <p>Contacter</p>
                     </a>
@@ -61,5 +61,96 @@ function injectNavbar() {
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
 }
 
-// Inject la navbar dans le body
-injectNavbar();
+
+/*
+* Injecte la navbar dans le body
+* 
+*/
+function injectMobileNavbar() {
+    const id = 'id="actual-page"'
+
+    // Récupère le nom de la page actuelle
+    const actualPath = window.location.pathname.split('/').pop().split('.')[0];
+
+    // Définit l'id de la page actuelle
+    let id1 = "", id2 = "", id3 = "", id4 = "";
+    switch (actualPath) {
+        case "contact":
+            break;
+        case "quisommenous":
+            id2 = id;
+            break;
+        case "action":
+            id3 = id;
+            break;
+        case "aide":
+            id4 = id;
+            break;
+        default:
+            id1 = id;
+            break;
+    }
+
+    const navbarHTML = `
+    <header>
+        <div class="off-screen-menu">
+            <ul>
+                <li ${id1}>
+                    <a href="./html/index.html">Acceuil</a>
+                </li>
+                <li ${id2}>
+                    <a href="./html/quisommenous.html">Qui somme-nous ?</a>
+                </li>
+                <li ${id3}>
+                    <a href="./html/action.html">Nos actions</a>
+                </li>
+                <li ${id4}>
+                    <a href="./html/aide.html">Nous aider</a>
+                </li>
+            </ul>
+        </div>
+        <nav>
+            <ul class="ham-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </ul>
+
+            <ul class="menu">
+                <a href="./html/index.html"><img src="./assets/Logo_Cadus_Mobile.svg" alt="logo du site" /></a>
+            </ul>
+
+            <ul class="contact-button">
+                <li>
+                    <a href="./html/contact.html">
+                        <img src="./assets/arrow-circle.png" alt="arrow image">
+                        <p>Contacter</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    `;
+
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+}
+
+
+
+if (window.innerWidth > 800) {
+    // Inject la navbar dans le body
+    injectDesktopNavbar();
+  }
+else {
+    // Inject la navbar dans le body
+    injectMobileNavbar();
+
+    const hamMenu = document.querySelector('.ham-menu');
+
+    const offScreenMenu = document.querySelector('.off-screen-menu');
+
+    hamMenu.addEventListener('click', () => {
+        hamMenu.classList.toggle('active');
+        offScreenMenu.classList.toggle('active');
+    });
+}
--- js/scrollBento.js
+++ js/scrollBento.js
@@ -1,156 +1,193 @@
+
+
 // test le plugin ScrollTrigger de gsap
 gsap.registerPlugin(ScrollTrigger);
 
-// Animation fixed position de la ssentence
-gsap.to(".sentence", {
-  scrollTrigger: {
-    trigger: ".sentence",
-    start: "top 26%",
-    end: "+=88%",
-    pin: true,
-  },
-});
-
-// Animation de la flèche
-// animation du haut de la flèche
-gsap.to("#arrow-top", {
-  width: "0em",
-  yPercent: 3000,
-  scrollTrigger: {
-    trigger: ".arrow",
-    start: "25% 30%",
-    end: "center 30%",
-    scrub: 0.5,
-  },
-});
-
-// animation du bas de la flèche
-gsap.to("#arrow-bottom", {
-  width: "0em",
-  yPercent: 3500,
-  scrollTrigger: {
-    trigger: ".arrow",
-    start: "center 30%",
-    end: "bottom-=10% 40%",
-    scrub: 0.5,
-  },
-});
-
-// animation de la rotation de la fleche de gauche
-gsap.to("#arrow-right", {
-  rotation: -90,
-  xPercent: -35,
-  yPercent: 75,
-  scrollTrigger: {
-    trigger: ".arrow",
-    start: "center 30%",
-    end: "bottom 55%",
-    scrub: 0.5,
-  },
-});
-// animation de la disparition de la fleche de gauche
-gsap.to("#arrow-right", {
-  opacity: 0,
-  scrollTrigger: {
-    trigger: ".arrow",
-    start: "bottom-=16% 47%",
-    end: "bottom-=10% 48%",
-    scrub: 0.5,
-  },
-});
-
-// animation de la rotation de la fleche de droite
-gsap.to("#arrow-left", {
-  rotation: 90,
-  xPercent: 30,
-  yPercent: 75,
-  scrollTrigger: {
-    trigger: ".arrow",
-    start: "center 30%",
-    end: "bottom 55%",
-    scrub: 0.5,
-  },
-});
-// animation de la disparition de la fleche de droite
-gsap.to("#arrow-left", {
-  opacity: 0,
-  scrollTrigger: {
-    trigger: ".arrow",
-    start: "bottom-=16% 47%",
-    end: "bottom-=10% 48%",
-    scrub: 0.5,
-  },
-});
-
-// animation du texte dans la fleche
-gsap.to(".arrow p", {
-  yPercent: 300,
-  scale: 0.5,
-  opacity: 0,
-  scrollTrigger: {
-    trigger: ".arrow",
-    start: "center 30%",
-    end: "bottom-=15% 30%",
-    scrub: 1,
-  },
-});
 
 
 
-// Animation des differents compatiments du bento
-gsap.to(".b1", {
-  xPercent: -250,
-  scrollTrigger: {
-    trigger: "#bento",
-    toggleActions: "complete reverse restart none",
-    start: "top-=100% 30%",
-    end: "bottom-=115% 50%",
-  },
-  ease: "ease-out",
-  duration: 0.5,
-});
-gsap.to(".b2", {
-  xPercent: 250,
-  scrollTrigger: {
-    trigger: "#bento",
-    toggleActions: "complete reverse restart none",
-    start: "top-=100% 30%",
-    end: "bottom-=105% 50%",
-  },
-  ease: "ease-out",
-  duration: 0.8,
-});
-gsap.to(".b3", {
-  yPercent: 150,
-  scrollTrigger: {
-    trigger: "#bento",
-    toggleActions: "complete reverse restart none",
-    start: "top-=100% 30%",
-    end: "bottom-=95% 50%",
-  },
-  ease: "ease-out",
-  duration: 0.7,
-});
-gsap.to(".b4", {
-  yPercent: 100,
-  scrollTrigger: {
-    trigger: "#bento",
-    toggleActions: "complete reverse restart none",
-    start: "top-=100% 30%",
-    end: "bottom-=80% 50%",
-  },
-  ease: "ease-out",
-  duration: 0.8,
-});
-gsap.to(".b5", {
-  xPercent: 200,
-  scrollTrigger: {
-    trigger: "#bento",
-    toggleActions: "complete reverse restart none",
-    start: "top-=100% 30%",
-    end: "bottom-=75% 50%",
-  },
-  ease: "ease-out",
-  duration: 0.85,
-});
+if (window.innerWidth > 800) {
+  runSentenceAnimations("+=88%");
+  runBentoAnimations();
+}
+else {
+  runSentenceAnimations("end 26%");
+}
+
+
+/**
+ * Registers the ScrollTrigger plugin with GSAP and defines a series of animations
+ * to be triggered based on scroll events.
+ *
+ * The animations include:
+ * - Pinning a sentence element in place while scrolling.
+ *
+ *
+ */
+function runSentenceAnimations(endString) {
+    // Animation fixed position de la ssentence
+    gsap.to(".sentence", {
+      scrollTrigger: {
+        trigger: ".sentence",
+        start: "top 26%",
+        end: endString,
+        pin: true,
+      },
+    });
+}
+
+
+/**
+ * Registers the ScrollTrigger plugin with GSAP and defines a series of animations
+ * to be triggered based on scroll events.
+ *
+ * The animations include:
+ * - Animating various compartments of a bento element.
+ * - Animating an arrow element.
+ *
+ *
+ */
+function runBentoAnimations() {
+
+    // Animation de la flèche
+  // animation du haut de la flèche
+  gsap.to("#arrow-top", {
+    width: "0em",
+    yPercent: 3000,
+    scrollTrigger: {
+      trigger: ".arrow",
+      start: "25% 30%",
+      end: "center 30%",
+      scrub: 0.5,
+    },
+  });
+
+  // animation du bas de la flèche
+  gsap.to("#arrow-bottom", {
+    width: "0em",
+    yPercent: 3500,
+    scrollTrigger: {
+      trigger: ".arrow",
+      start: "center 30%",
+      end: "bottom-=10% 40%",
+      scrub: 0.5,
+    },
+  });
+
+  // animation de la rotation de la fleche de gauche
+  gsap.to("#arrow-right", {
+    rotation: -90,
+    xPercent: -35,
+    yPercent: 75,
+    scrollTrigger: {
+      trigger: ".arrow",
+      start: "center 30%",
+      end: "bottom 55%",
+      scrub: 0.5,
+    },
+  });
+  // animation de la disparition de la fleche de gauche
+  gsap.to("#arrow-right", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".arrow",
+      start: "bottom-=16% 47%",
+      end: "bottom-=10% 48%",
+      scrub: 0.5,
+    },
+  });
+
+  // animation de la rotation de la fleche de droite
+  gsap.to("#arrow-left", {
+    rotation: 90,
+    xPercent: 30,
+    yPercent: 75,
+    scrollTrigger: {
+      trigger: ".arrow",
+      start: "center 30%",
+      end: "bottom 55%",
+      scrub: 0.5,
+    },
+  });
+  // animation de la disparition de la fleche de droite
+  gsap.to("#arrow-left", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".arrow",
+      start: "bottom-=16% 47%",
+      end: "bottom-=10% 48%",
+      scrub: 0.5,
+    },
+  });
+
+  // animation du texte dans la fleche
+  gsap.to(".arrow p", {
+    yPercent: 300,
+    scale: 0.5,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".arrow",
+      start: "center 30%",
+      end: "bottom-=15% 30%",
+      scrub: 1,
+    },
+  });
+
+  // Animation des differents compatiments du bento
+  gsap.to(".b1", {
+    xPercent: -250,
+    scrollTrigger: {
+      trigger: "#bento",
+      toggleActions: "complete reverse restart none",
+      start: "top-=100% 30%",
+      end: "bottom-=115% 50%",
+    },
+    ease: "ease-out",
+    duration: 0.5,
+  });
+  gsap.to(".b2", {
+    xPercent: 250,
+    scrollTrigger: {
+      trigger: "#bento",
+      toggleActions: "complete reverse restart none",
+      start: "top-=100% 30%",
+      end: "bottom-=105% 50%",
+    },
+    ease: "ease-out",
+    duration: 0.8,
+  });
+  gsap.to(".b3", {
+    yPercent: 150,
+    scrollTrigger: {
+      trigger: "#bento",
+      toggleActions: "complete reverse restart none",
+      start: "top-=100% 30%",
+      end: "bottom-=95% 50%",
+    },
+    ease: "ease-out",
+    duration: 0.7,
+  });
+  gsap.to(".b4", {
+    yPercent: 100,
+    scrollTrigger: {
+      trigger: "#bento",
+      toggleActions: "complete reverse restart none",
+      start: "top-=100% 30%",
+      end: "bottom-=80% 50%",
+    },
+    ease: "ease-out",
+    duration: 0.8,
+  });
+  gsap.to(".b5", {
+    xPercent: 200,
+    scrollTrigger: {
+      trigger: "#bento",
+      toggleActions: "complete reverse restart none",
+      start: "top-=100% 30%",
+      end: "bottom-=75% 50%",
+    },
+    ease: "ease-out",
+    duration: 0.85,
+  });
+}
 
